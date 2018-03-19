f8031a3e5e988896a7d8ba1043ef715ba90399c4
Subha Singh <singh.subha22@gmail.com>
2018-03-19 16:12:33 +0200

    End of chapter 6

--- MvcMusicStore/Models/Order.cs
+++ MvcMusicStore/Models/Order.cs
@@ -1,25 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DataAnnotationsValidations.Attributes;
+using System;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+
 
 namespace MvcMusicStore.Models
 {
     public class Order
     {
+        [Display(Name = "Order Id")]
         public int OrderId { get; set; }
+        [Display(Name ="Order Date")]
         public DateTime OrderDate { get; set; }
         public string Username { get; set; }
         [Required]
         [StringLength(160, MinimumLength=3)]
+        [Display(Name = "Last Name", Order = 15000)]
         public string FirstName { get; set; }
         [Required(ErrorMessage ="Your {(0)} is required.")]
-        [StringLength(160, ErrorMessage ="Your {(0)} is too long.")]
+        [StringLength(160)]
+        [MaxWords(10, ErrorMessage = "There are too many words in {0}")]
+        [Display(Name = "Last Name", Order = 15001) ]
         public string LastName { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
+        [Display(Name = "Postal Code")]
         public string PostalCode { get; set; }
         public string Country { get; set; }
         public string Phone { get; set; }
@@ -29,4 +34,6 @@ namespace MvcMusicStore.Models
         [Range(typeof(decimal), "0.00", "49.99")]
         public decimal Total { get; set; }
     }
+
+
 }
